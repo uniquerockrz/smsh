719421d4e1eb879454a046977467cdce49b7f5ff
Krishnashish Gogoi <mister_raindrop@lavabit.com>
2013-07-24 07:54:20 +0530

    Fixed indents in main.c

--- source/main.c
+++ source/main.c
@@ -25,39 +25,39 @@ int main (int argc, char *argv[])
 	
 	// Starting banner and stuff
 	printf("Starting smsh - the sms shell for Linux remote computing\n");
-    printf("===========================================================\n\n");
-    printf("This is a very crude and rude version of the program.\n");
-    printf("It sometimes works, it sometimes doesn't. DO NOT COMPLAIN.\n");
-    printf("Original Author: Saurav Modak.\n\n");
-    printf("... Krishnashish Gogoi ...\n");
+	printf("===========================================================\n\n");
+	printf("This is a very crude and rude version of the program.\n");
+	printf("It sometimes works, it sometimes doesn't. DO NOT COMPLAIN.\n");
+	printf("Original Author: Saurav Modak.\n\n");
+	printf("... Krishnashish Gogoi ...\n");
     
-    // Sets the current time in a certain format in lastmsg.txt.
-    // This is later used in analyze.c to compare time.
-    // Any time before starting the program is irrelevant,
-    // hence the starting time.
-    time_t curtime;
-    struct tm *loctime;
-    char setDate[30];
-    FILE *fp;
-    fp = fopen("lastmsg.txt", "w");
-    curtime=time(NULL);
-    loctime= localtime(&curtime);
+	// Sets the current time in a certain format in lastmsg.txt.
+	// This is later used in analyze.c to compare time.
+	// Any time before starting the program is irrelevant,
+	// hence the starting time.
+	time_t curtime;
+	struct tm *loctime;
+	char setDate[30];
+	FILE *fp;
+	fp = fopen("lastmsg.txt", "w");
+	curtime=time(NULL);
+	loctime= localtime(&curtime);
     
     
     
-    // Writing current date and time to lastmsg.txt
-    // with a proper format for compareTime()
-    strftime(setDate, 30, "%d %b %Y %H:%M:%S", loctime);
-    fputs(setDate, fp);
-    fclose(fp);
+	// Writing current date and time to lastmsg.txt
+	// with a proper format for compareTime()
+	strftime(setDate, 30, "%d %b %Y %H:%M:%S", loctime);
+	fputs(setDate, fp);
+	fclose(fp);
     
-    char lineBuff[30];
-    char urlBuff[80] = "http://tagg.in/Rss/";
-    int urlLength = strlen(urlBuff);
-    int loopFlag = 1;
+	char lineBuff[30];
+	char urlBuff[80] = "http://tagg.in/Rss/";
+	int urlLength = strlen(urlBuff);
+	int loopFlag = 1;
     
-    // Reads input. Silly little loop.
-    do {
+	// Reads input. Silly little loop.
+	do {
 		printf("\n\nEnter your tagg.in username: ");
     
 		// Read Input 
@@ -70,16 +70,16 @@ int main (int argc, char *argv[])
 	} while (loopFlag == 1);
     
     
-    // Main program loop. Calls start(), waits WAIT no. of secs,
-    // calls again. Silly I know, but an improvement over the
-    // previous recursion I believe. 
-    while (iterflag) {
+	// Main program loop. Calls start(), waits WAIT no. of secs,
+	// calls again. Silly I know, but an improvement over the
+	// previous recursion I believe. 
+	while (iterflag) {
 		start(urlBuff);
 		printf("Waiting %d seconds for the next check\n\n", WAIT);
-        sleep(WAIT);
+		sleep(WAIT);
 	}
     
-    return 0;
+	return 0;
 }
 	
 
